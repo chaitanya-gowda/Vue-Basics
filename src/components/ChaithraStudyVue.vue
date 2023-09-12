61b8d669773fbd8d1635742d499ff8aa6053c6ce
chaitanya-gowda <142878497+chaitanya-gowda@users.noreply.github.com>
2023-09-12 11:02:59 +0530

    Added computed and watchers

--- src/components/ChaithraStudyVue.vue
+++ src/components/ChaithraStudyVue.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <!-- <h6 class="user">ref : {{ users }}</h6> -->
-    <h6 class="user">reactive : {{ reactiveUser.name }}</h6>
-    <h6 class="user">ref : {{ user.name }}</h6>
+    <!-- <h6 class="user">reactive : {{ reactiveUser.name }}</h6>-->
+    <!-- <h6 class="user">ref : {{ user.name }}</h6>
+   
+    <h6 class="user">Computed : {{ computedData }}</h6> -->
+    <h6 class="user">Watch : {{ watchData }}</h6>
+    <!-- <input type="text" placeholder="enter name" v-model="compUser" /> -->
+    <button @click="setName">Methods : SetUserName</button>
   </div>
 </template>
 
@@ -22,6 +27,12 @@ export default {
       age: -2,
     });
 
+    //computed
+    const compUser = ref('');
+
+    // watch
+    const watchData = ref('thanu');
+
     // reactive
 
     const reactiveUser = reactive({
@@ -29,27 +40,57 @@ export default {
       age: 27,
     });
     console.log('ref value', users.value);
-    setTimeout(() => {
-      users.value = users.value.map((value) => value + ' loose');
+    // setTimeout(() => {
+    //   users.value = users.value.map((value) => value + ' loose');
 
-      //reactiveUser
-      reactiveUser.name = reactiveUser.name + ' mentlu';
-      reactiveUser.age = 28;
+    //   //reactiveUser
+    //   reactiveUser.name = reactiveUser.name + ' mentlu';
+    //   reactiveUser.age = 28;
 
-      // refUser
-      user.value.name = user.value.name + ' Hahaha';
-    }, 2000);
+    //   // refUser
+    //   user.value.name = user.value.name + ' Hahaha';
+    // }, 2000);
 
     // const computedValue: computed(()=>{
     //   return reactiveUser.
 
     // })
 
-    // watch(user.value, (oldValue, newValue) => {
-    //   console.log('old value', oldValue);
-    //   console.log('new value', newValue);
-    // });
-    return { users, reactiveUser, user };
+    // watch
+    watch(watchData, function (newValue, oldValue) {
+      console.log('old value', oldValue);
+      console.log('new value', newValue);
+    });
+
+    //methods
+
+    function setName() {
+      console.log('clicked');
+      // user.value.name = 'thanu';
+      watchData.value = 'chaithra';
+    }
+    // function setOnChange(event) {
+    //   compUser.value = event.target.value;
+    // }
+
+    // computed
+    // computed properties are read only.. cannot able to overwrite the value
+
+    const computedData = computed(function () {
+      return compUser.value;
+    });
+    console.log('userdata', computedData);
+
+    // setup return
+    return {
+      users,
+      reactiveUser,
+      user,
+      setName,
+      compUser,
+      computedData,
+      watchData,
+    };
   },
 };
 </script>
